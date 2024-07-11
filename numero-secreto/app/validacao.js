a0ea152b92bcfb3337cb363c5bcec44792338bef
Damyres <damyresmaciell@gmail.com>
2024-07-11 20:06:15 -0300

    Exibir mensagem se o número falado é maior ou menor do que o gerado

--- numero-secreto/app/validacao.js
+++ numero-secreto/app/validacao.js
@@ -3,18 +3,30 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `<div>
             Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
             `
+        return
     }
 
-    if(numero === numeroSecreto){
+    if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div> 
+        </div> 
+        `
+    } else{
+        elementoChute.innerHTML += ` 
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div> 
+        </div> 
         `
     }
 }
